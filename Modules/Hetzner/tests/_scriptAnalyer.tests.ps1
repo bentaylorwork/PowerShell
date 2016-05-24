cb974a6f1466061926e1d8914dd2747214b98855
Ben Taylor <bentaylorwork@users.noreply.github.com>
2016-05-24 17:03:46 +0200

    Update _scriptAnalyer.tests.ps1

--- Modules/Hetzner/tests/_scriptAnalyer.tests.ps1
+++ Modules/Hetzner/tests/_scriptAnalyer.tests.ps1
@@ -1,25 +1,31 @@
-﻿$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace('tests', '')
+$here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace('tests', '')
 $scriptsModules = Get-ChildItem $here -Include *.psd1, *.psm1, *.ps1 -Exclude *.tests.ps1 -Recurse
 
-if(($scriptsModules.count -ne 0) -and (Get-Command Invoke-ScriptAnalyzer -errorAction SilentlyContinue)) {
-	$scriptAnalyzerRules = Get-ScriptAnalyzerRule
-	Describe 'INFO: General - Testing all scripts and modules conform to the Script Analyzer Rules' {
-		forEach ($scriptModule in $scriptsModules) {
-			switch ($scriptModule) { 
-				'*.psm1' { $typeTesting = 'Module' } 
-				'*.ps1'  { $typeTesting = 'Script' } 
-				'*.psd1' { $typeTesting = 'Manifest' } 
-			}
+Describe 'General - Testing all scripts and modules conform to the Script Analyzer Rules' {
+    Context "Checking files to test exist and Invoke-ScriptAnalyzer cmdLet is available" {
+        It "Checking files exist to test." {
+            $scriptsModules.count | Should Not Be 0
+        }
+        It "Checking Invoke-ScriptAnalyzer exists." {
+            { Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should Not Throw
+        }
+    }
 
-			Context "INFO: Checking $typeTesting – $($module.BaseName) conforms to Script Analyzer Rules" {
-				forEach ($scriptAnalyzerRule in $scriptAnalyzerRules) {
-					It "INFO: It passes Script Analyzer Rule: $scriptAnalyzerRule" {
-						(Invoke-ScriptAnalyzer -Path $scriptModule.fullName -IncludeRule $scriptAnalyzerRule.ruleName ).Count | Should Be 0
-					}
+    $scriptAnalyzerRules = Get-ScriptAnalyzerRule
+
+	forEach ($scriptModule in $scriptsModules) {
+		switch -wildCard ($scriptModule) { 
+			'*.psm1' { $typeTesting = 'Module' } 
+			'*.ps1'  { $typeTesting = 'Script' } 
+			'*.psd1' { $typeTesting = 'Manifest' } 
+		}
+
+		Context "Checking $typeTesting – $($scriptModule) conforms to Script Analyzer Rules" {
+			forEach ($scriptAnalyzerRule in $scriptAnalyzerRules) {
+				It "Script Analyzer Rule: $scriptAnalyzerRule" {
+					(Invoke-ScriptAnalyzer -Path $scriptModule.fullName -IncludeRule $scriptAnalyzerRule.ruleName ).Count | Should Be 0
 				}
 			}
 		}
 	}
-} else {
-	Write-Error -Message 'ERROR: No files found to test or Invoke-ScriptAnalyzer not installed.'
 }
