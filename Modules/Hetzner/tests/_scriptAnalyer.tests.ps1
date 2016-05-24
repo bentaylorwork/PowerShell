4d1ab8999b9daf9d9051bffaeb5960c732ef0178
Ben Taylor <bentaylorwork@users.noreply.github.com>
2016-05-24 23:56:53 +0200

    Update _scriptAnalyer.tests.ps1

--- Modules/Hetzner/tests/_scriptAnalyer.tests.ps1
+++ Modules/Hetzner/tests/_scriptAnalyer.tests.ps1
@@ -1,17 +1,17 @@
 $here = (Split-Path -Parent $MyInvocation.MyCommand.Path).Replace('tests', '')
 $scriptsModules = Get-ChildItem $here -Include *.psd1, *.psm1, *.ps1 -Exclude *.tests.ps1 -Recurse
 
-Describe 'General - Testing all scripts and modules conform to the Script Analyzer Rules' {
-    Context "Checking files to test exist and Invoke-ScriptAnalyzer cmdLet is available" {
-        It "Checking files exist to test." {
-            $scriptsModules.count | Should Not Be 0
-        }
-        It "Checking Invoke-ScriptAnalyzer exists." {
-            { Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should Not Throw
-        }
-    }
+Describe 'General - Testing all scripts and modules against the Script Analyzer Rules' {
+	Context "Checking files to test exist and Invoke-ScriptAnalyzer cmdLet is available" {
+		It "Checking files exist to test." {
+			$scriptsModules.count | Should Not Be 0
+		}
+		It "Checking Invoke-ScriptAnalyzer exists." {
+			{ Get-Command Invoke-ScriptAnalyzer -ErrorAction Stop } | Should Not Throw
+		}
+	}
 
-    $scriptAnalyzerRules = Get-ScriptAnalyzerRule
+	$scriptAnalyzerRules = Get-ScriptAnalyzerRule
 
 	forEach ($scriptModule in $scriptsModules) {
 		switch -wildCard ($scriptModule) { 
@@ -20,10 +20,10 @@ Describe 'General - Testing all scripts and modules conform to the Script Analyz
 			'*.psd1' { $typeTesting = 'Manifest' } 
 		}
 
-		Context "Checking $typeTesting – $($scriptModule) conforms to Script Analyzer Rules" {
+		Context "Checking $typeTesting – $($scriptModule) - conforms to Script Analyzer Rules" {
 			forEach ($scriptAnalyzerRule in $scriptAnalyzerRules) {
 				It "Script Analyzer Rule: $scriptAnalyzerRule" {
-					(Invoke-ScriptAnalyzer -Path $scriptModule.fullName -IncludeRule $scriptAnalyzerRule.ruleName ).Count | Should Be 0
+					(Invoke-ScriptAnalyzer -Path $scriptModule -IncludeRule $scriptAnalyzerRule).count | Should Be 0
 				}
 			}
 		}
