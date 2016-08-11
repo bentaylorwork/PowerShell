4ac8fb43b29e0db6fd2658bc368f3d538f903c20
Ben Taylor <Ben Taylor@PC>
2016-08-11 17:49:06 +0200

    Updates

--- Scripts/Inventory/Get-ComputerFreeSpace.ps1
+++ Scripts/Inventory/Get-ComputerFreeSpace.ps1
@@ -1,97 +1,93 @@
 function Get-ComputerFreeSpace () {
-    <# 
-        .SYNOPSIS 
-            A function to query free disk space.
-        .EXAMPLE
-            Import-Module virtualmachinemanager
-            $computerNames = (Get-ScVirtualMachine | Where-Object { ($_.Status –eq 'Running') }).name
-            Get-ComputerFreeSpace -computerName $computerNames -Verbose
-        .EXAMPLE
-            $computers = @('computerOne', 'computerTwo', 'computerThree')
-            Get-ComputerFreeSpace -computerName $computers
-        .NOTES
-            Written by Ben Taylor
-            Version 1.0, 05.04.2016
-    #>
-    [CmdletBinding()]
-    [OutputType([System.Collections.ArrayList])]
-    param (
-        [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline=$True, valuefrompipelinebypropertyname=$true)]
-        [ValidateNotNull()]
-        [ValidateNotNullOrEmpty()]
-        [Alias('IPAddress', 'Name')]
-        [String[]]$computerName = $env:COMPUTERNAME,
-        [Parameter(Mandatory = $false, Position = 1)]
-        [ValidateSet('DCOM', 'WSMan', 'Auto')] 
-        [String] $Protocol = 'Auto',
-        [Parameter(Mandatory = $false, Position = 2)]
-        [System.Management.Automation.PSCredential]
-        [System.Management.Automation.Credential()]
-        $Credential
-    )
+	<# 
+		.SYNOPSIS 
+			A function to query free disk space.
+		.EXAMPLE
+			Import-Module virtualmachinemanager
+			$computerNames = (Get-ScVirtualMachine | Where-Object { ($_.Status –eq 'Running') }).name
+			Get-ComputerFreeSpace -computerName $computerNames -Verbose
+		.EXAMPLE
+			$computers = @('computerOne', 'computerTwo', 'computerThree')
+			Get-ComputerFreeSpace -computerName $computers
+		.NOTES
+			Written by Ben Taylor
+			Version 1.1, 08.10.2016
+	#>
+	[CmdletBinding()]
+	[OutputType([System.Object])]
+	param (
+		[Parameter(Mandatory = $false, Position = 0, ValueFromPipeline=$True, valuefrompipelinebypropertyname=$true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('IPAddress', 'Name')]
+		[String[]]$computerName = $env:COMPUTERNAME,
+		[Parameter(Mandatory = $false, Position = 1)]
+		[ValidateSet('DCOM', 'WSMan', 'Auto')] 
+		[String] $Protocol = 'Auto',
+		[Parameter(Mandatory = $false, Position = 2)]
+		[System.Management.Automation.PSCredential]
+		[System.Management.Automation.Credential()]
+		$Credential
+	)
 
-    Begin {
-        $returnArray = New-Object System.Collections.ArrayList
+	Begin {
+		$cimCommonParams = @{
+			ErrorAction = 'Stop'
+		}
 
-        $cimCommonParams = @{
-            ErrorAction = 'Stop'
-        }
+		If ($PSBoundParameters['Credential']) {
+			$cimCommonParams.Credential = $Credential
+		}
+	} Process {
+		forEach ($computer in $computerName) {
+			try {
+				$cimCommonParams.ComputerName = $computer
 
-        If ($PSBoundParameters['Credential']) {
-            $cimCommonParams.Credential = $Credential
-        }
-    } Process {
-        forEach ($computer in $computerName) {
-            try {
-                $cimCommonParams.ComputerName = $computer
+				Write-Verbose "$computer - Trying to Get Disk Space"
+				if($Protocol -eq 'Auto') {
+					Try {
+						Test-WSMan @cimCommonParams | Out-Null
+						$ProtocolAuto = 'WsMan'
+					} Catch {
+						$ProtocolAuto = 'DCOM'
+					}
+				} else {
+					$ProtocolAuto =  $Protocol
+				}
 
-                Write-Verbose "INFO: $computer - Trying to Get Disk Space"
-                if($Protocol -eq 'Auto') {
-                    Try {
-                        Test-WSMan @cimCommonParams | Out-Null
-                        $ProtocolAuto = 'WsMan'
-                    } Catch {
-                        $ProtocolAuto = 'DCOM'
-                    }
-                } else {
-                    $ProtocolAuto =  $Protocol
-                }
+				$cimCommonParams.SessionOption = (New-CimSessionOption -Protocol $ProtocolAuto)
 
-                $cimCommonParams.SessionOption = (New-CimSessionOption -Protocol $ProtocolAuto)
+				$cimSession = New-CimSession @cimCommonParams
 
-                $cimSession = New-CimSession @cimCommonParams
+				#Get Free disk space
+				$disks = Get-CimInstance Win32_LogicalDisk -Filter "DriveType = 3" -CimSession $cimSession -ErrorAction Stop
 
-                #Get Free disk space
-                $disks = Get-CimInstance Win32_LogicalDisk -Filter "DriveType = 3" -CimSession $cimSession -ErrorAction Stop
+				Write-Verbose "$computer - Successfully queried Disk space Info"
 
-                Write-Verbose "INFO: $computer - Successfully queried Disk space Info"
+				if($disks) {
+					Write-Verbose "$computer - Disk info found"
+					forEach ($disk in $disks) {
+						#Build Object To Return Info
+						$returnObj = New-Object System.Object
 
-                if($disks) {
-                    Write-Verbose "INFO: $computer - Disk info found"
-                    forEach ($disk in $disks) {
-                        #Build Object To Return Info
-                        $returnObj = New-Object System.Object
+						$returnObj | Add-Member -type NoteProperty -name COMPUTERNAME -Value $computer -force
+						$returnObj | Add-Member -type NoteProperty -name DISK -Value $disk.DeviceID -force
+						$returnObj | Add-Member -type NoteProperty -name VOLUMENAME -Value $disk.VolumeName -force
+						$returnObj | Add-Member -type NoteProperty -name TOTALSPACEBYTES -Value $disk.Size -force
+						$returnObj | Add-Member -type NoteProperty -name TOTALSPACEGB -Value ([math]::Round(($disk.Size / 1GB), 2)) -force
+						$returnObj | Add-Member -type NoteProperty -name FREESPACEBYTES -Value $disk.FreeSpace -force
+						$returnObj | Add-Member -type NoteProperty -name FREESPACEGB -Value ([math]::Round(($disk.FreeSpace / 1GB), 2)) -force
+						$returnObj | Add-Member -type NoteProperty -name FREESPACEPERCENT -Value ([math]::Round((($disk.FreeSpace / $disk.Size) * 100), 2)) -force
 
-                        $returnObj | Add-Member -type NoteProperty -name COMPUTERNAME -Value $computer -force
-                        $returnObj | Add-Member -type NoteProperty -name DISK -Value $disk.DeviceID -force
-                        $returnObj | Add-Member -type NoteProperty -name VOLUMENAME -Value $disk.VolumeName -force
-                        $returnObj | Add-Member -type NoteProperty -name TOTALSPACEBYTES -Value $disk.Size -force
-                        $returnObj | Add-Member -type NoteProperty -name TOTALSPACEGB -Value ([math]::Round(($disk.Size / 1GB), 2)) -force
-                        $returnObj | Add-Member -type NoteProperty -name FREESPACEBYTES -Value $disk.FreeSpace -force
-                        $returnObj | Add-Member -type NoteProperty -name FREESPACEGB -Value ([math]::Round(($disk.FreeSpace / 1GB), 2)) -force
-                        $returnObj | Add-Member -type NoteProperty -name FREESPACEPERCENT -Value ([math]::Round((($disk.FreeSpace / $disk.Size) * 100), 2)) -force
-
-                        $returnArray.Add($returnObj) | Out-Null                     
-                    }
-                }
-            } Catch {
-                Write-Error "ERROR: $computer - Could not query disk space info."
-            } finally {
-                if($null -ne $cimSession) {
-                    Remove-CimSession $cimSession
-                }
-            }
-        }
-        $returnArray
-    }
+						$returnObj                    
+					}
+				}
+			} Catch {
+				Write-Error "$computer - $_"
+			} finally {
+				if($null -ne $cimSession) {
+					Remove-CimSession $cimSession -ErrorAction SilentlyContinue
+				}
+			}
+		}
+	}
 }
--- Scripts/Inventory/Get-ServiceRunAsAccount.ps1
+++ Scripts/Inventory/Get-ServiceRunAsAccount.ps1
@@ -1,94 +1,87 @@
 function Get-ServiceRunAsAccount {
-    <# 
-        .SYNOPSIS 
-            A function to query services running under a certain account.
-        .EXAMPLE
-            Import-Module virtualmachinemanager
-            $computerNames = (Get-ScVirtualMachine | Where-Object { ($_.Status –eq 'Running') }).name
-            Get-ServiceRunAsAccount -computerName $computerNames -Verbose
-        .EXAMPLE
-            Get-ServiceRunAsAccount -computerName 'computerOne', 'computerTwo' -serviceAccount LocalSystem -Verbose
-        .EXAMPLE
-            Import-Module ActiveDirectory
-            (Get-ADComputer -Filter *).name | Get-ServiceRunAsAccount -serviceAccount LocalSystem -verbose -Protocol DCOM
-        .EXAMPLE
-            Get-ServiceRunAsAccount
-        .EXAMPLE
-            Get-ServiceRunAsAccount -Credential (Get-Credential)
-        .NOTES
-            Written by Ben Taylor
-            Version 1.0, 21.04.2016
-    #>
-    [CmdletBinding()]
-    [OutputType([System.Collections.ArrayList])]
-    param (
-        [Parameter(Mandatory = $false, Position = 0, ValueFromPipeline=$True, valuefrompipelinebypropertyname=$true)]
-        [ValidateNotNull()]
-        [ValidateNotNullOrEmpty()]
-        [Alias('IPAddress', 'Name')]
-        [string[]]$computerName = $env:COMPUTERNAME,
-        [Parameter(Mandatory = $false, Position = 1)]
-        [ValidateNotNull()]
-        [String] $serviceAccount = 'Administrator',
-        [Parameter(Mandatory = $false, Position = 2)]
-        [ValidateSet('DCOM', 'WinRM', 'Auto')] 
-        [String] $Protocol = 'Auto',
-        [Parameter(Mandatory = $false, Position = 3)]
-        [System.Management.Automation.PSCredential]
-        [System.Management.Automation.Credential()]
-        $Credential
-    )
+	<# 
+		.SYNOPSIS 
+			A function to query services running under a certain account.
+		.EXAMPLE
+			Import-Module virtualmachinemanager
+			$computerNames = (Get-ScVirtualMachine | Where-Object { ($_.Status –eq 'Running') }).name
+			Get-ServiceRunAsAccount -computerName $computerNames -Verbose
+		.EXAMPLE
+			Get-ServiceRunAsAccount -computerName 'computerOne', 'computerTwo' -serviceAccount LocalSystem -Verbose
+		.EXAMPLE
+			Import-Module ActiveDirectory
+			(Get-ADComputer -Filter *).name | Get-ServiceRunAsAccount -serviceAccount LocalSystem -verbose -Protocol DCOM
+		.EXAMPLE
+			Get-ServiceRunAsAccount
+		.EXAMPLE
+			Get-ServiceRunAsAccount -Credential (Get-Credential)
+		.NOTES
+			Written by Ben Taylor
+			Version 1.1, 25.04.2016
+	#>
+	[CmdletBinding()]
+	[OutputType([System.Object])]
+	param (
+		[Parameter(Mandatory = $false, Position = 0, ValueFromPipeline=$True, valuefrompipelinebypropertyname=$true)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('IPAddress', 'Name')]
+		[string[]]$computerName = $env:COMPUTERNAME,
+		[Parameter(Mandatory = $false, Position = 1)]
+		[ValidateNotNull()]
+		[String] $serviceAccount = 'Administrator',
+		[Parameter(Mandatory = $false, Position = 2)]
+		[ValidateSet('DCOM', 'WsMAN', 'Auto')] 
+		[String] $Protocol = 'Auto',
+		[Parameter(Mandatory = $false, Position = 3)]
+		[System.Management.Automation.PSCredential]
+		[System.Management.Automation.Credential()]
+		$Credential
+	)
 
-    Begin {
-        $returnArray = New-Object System.Collections.ArrayList
+	Begin {
+		$cimCommonParams = @{
+			ErrorAction = 'Stop'
+		}
 
-        $cimCommonParams = @{
-            ErrorAction = 'Stop'
-        }
+		If ($PSBoundParameters['Credential']) {
+			$cimCommonParams.Credential = $Credential
+		}
+	} Process {
+		forEach ($computer in $computerName) {
+			try {
+				$cimCommonParams.ComputerName = $computer
 
-        If ($PSBoundParameters['Credential']) {
-            $cimCommonParams.Credential = $Credential
-        }
-    } Process {
-        forEach ($computer in $computerName) {
-            try {
-                $cimCommonParams.ComputerName = $computer
+				if($Protocol -eq 'Auto') {
+					Try {
+						Test-WSMan @cimCommonParams | Out-Null
+						$ProtocolAuto = 'WsMAN'
+					} Catch {
+						$ProtocolAuto = 'DCOM'
+					}
+				} else {
+					$ProtocolAuto =  $Protocol
+				}
 
-                if($Protocol -eq 'Auto') {
-                    Try {
-                        Test-WSMan @cimCommonParams
-                        $ProtocolAuto = 'WinRM'
-                    } Catch {
-                        $ProtocolAuto = 'DCOM'
-                    }
-                } else {
-                    $ProtocolAuto =  $Protocol
-                }
-                
-                $cimSession = $null
+				$cimCommonParams.SessionOption = (New-CimSessionOption -Protocol $ProtocolAuto)
 
-                $cimCommonParams.SessionOption = (New-CimSessionOption -Protocol $ProtocolAuto)
+				$cimSession = New-CimSession @cimCommonParams
 
-                $cimSession = New-CimSession @cimCommonParams
-                $services = Get-CimInstance -CimSession $cimSession -ClassName win32_service
+				$services = Get-CimInstance -CimSession $cimSession -ClassName win32_service
 
-                forEach ($service in $services) {
-                    If ($service.startName -contains $serviceAccount) {
+				forEach ($service in $services) {
+					If ($service.startName -contains $serviceAccount) {
+						Write-Verbose "The service $service.Name on $computer is running as $serviceAccount"
 
-                        $returnArray.Add($service) | Out-Null
-
-                        Write-Verbose "INFO: The service $service.Name on $computer is running as $serviceAccount"
-                    }
-                }
-            } catch {
-                Write-Error "ERROR: Couldn't Query $computer."
-            } finally {
-                if($null -ne $cimSession) {
-                    Remove-CimSession $cimSession
-                }
-            }
-        }
-    } End {
-        $returnArray
-    } 
+						$service
+					}
+				}
+			} catch {
+				Write-Error "$computer - $_"
+			} finally {
+				if($null -ne $cimSession) {
+					Remove-CimSession $cimSession -ErrorAction SilentlyContinue
+				}
+			}
+		}
+	}
 }
