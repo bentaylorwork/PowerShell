98b377a2688be804d6357552707d1c444d07869d
Ben Taylor <bentaylorwork@users.noreply.github.com>
2017-03-01 12:46:21 +0100

    Update Enable-ReplicationForVMsOnHost.ps1

--- Scripts/Hyper-V/Enable-ReplicationForVMsOnHost.ps1
+++ Scripts/Hyper-V/Enable-ReplicationForVMsOnHost.ps1
@@ -1,18 +1,16 @@
 function Enable-ReplicationForVMsOnHost
 {
     <#
-    .Synopsis
-       Enables replication for a all VMs that are not replicated on a host to another host.
-    .EXAMPLE
-       Enable-ReplicationForVMsOnHost -sourceHost 'hyperv-one' -desinationhost 'hyper-two'
-    .NOTES
-        Written by Ben Taylor
-        Version 1.0, 24.02.2017
+        .Synopsis
+            Enables replication for all VMs that are not replicated on a host to another host.
+        .EXAMPLE
+            Enable-ReplicationForVMsOnHost -sourceHost 'hyperv-one' -desinationhost 'hyper-two'
+        .NOTES
+            Written by Ben Taylor
+            Version 1.1, 01.03.2017
     #>
-    [CmdletBinding(DefaultParameterSetName='Parameter Set 1', 
-                  SupportsShouldProcess=$true,
-                  ConfirmImpact='Medium')]
-    [OutputType([String])]
+    [CmdletBinding(SupportsShouldProcess=$true, ConfirmImpact='Medium')]
+    [OutputType()]
     Param
     (
         [Parameter(Mandatory=$true, Position=0)]
@@ -29,53 +27,51 @@ function Enable-ReplicationForVMsOnHost
         $timeOut = 7200
     )
 
-    Begin
-    {
-        if($sourceHost -eq $destinationHost) {
-            Throw "Source and Destination Host The Same"
-        }
+    # Prefer here as error message more informative than validateScript
+    if($sourceHost -eq $destinationHost) {
+        Throw "Source and Destination Host The Same"
     }
-    Process
+
+    $vms = Get-VM -ComputerName $sourceHost | Where-Object ReplicationState -EQ Disabled
+
+    foreach ($vm in $vms)
     {
-        $vms = Get-VM -ComputerName $sourceHost | Where-Object ReplicationState -EQ Disabled
-
-        foreach ($vm in $vms)
+        if ($pscmdlet.ShouldProcess($vm.name, 'Replicate'))
         {
-            if ($pscmdlet.ShouldProcess($vm.name, 'Replicate'))
+            try
             {
-                try
+                Write-Verbose "Enabling Replication on: $($vm.name)"
+                Enable-VMReplication -ComputerName $sourceHost $vm.name $destinationHost 80 Kerberos
+
+                Write-Verbose "Starting Initial Replication On vm: $($vm.name)"
+                Start-VMInitialReplication -ComputerName $sourceHost –VMName $vm.name
+
+                Start-Sleep 5
+
+                $timer = [Diagnostics.Stopwatch]::StartNew()
+
+                while ($vm.replicationstate -ne "Replicating" )
                 {
-                    Write-Verbose "Enabling Replication on: $($vm.name)"
-                    Enable-VMReplication -ComputerName $sourceHost $vm.name $destinationHost 80 Kerberos
-
-                    Write-Verbose "Starting Initial Replication On vm: $($vm.name)"
-                    Start-VMInitialReplication -ComputerName $sourceHost –VMName $vm.name
-
-                    Start-Sleep 5
-
-                    $timer = [Diagnostics.Stopwatch]::StartNew()
-
-                    while ($vm.replicationstate -ne "Replicating" )
+                    if($timer.Elapsed.TotalSeconds -ge $timeOut)
                     {
-                        if($timer.Elapsed.TotalSeconds -ge $timeOut)
-                        {
-                            Write-Error "$($vm.name) - Hit Timeout Limit So Skipping"
+                        Write-Error "$($vm.name) - Hit Timeout Limit, cancelling intial replication"
 
-                            break
-                        }
+                        Stop-VMInitialReplication -ComputerName $sourceHost -VMName $vm.name
 
-                        Start-Sleep 20
+                        break
                     }
 
+                    Start-Sleep 20
                 }
-                catch
-                {
-                    Write-Error $_
-                }
-                finally
-                {
-                    $timer.Stop()
-                }
+
+            }
+            catch
+            {
+                Write-Error $_
+            }
+            finally
+            {
+                $timer.Stop()
             }
         }
     }
